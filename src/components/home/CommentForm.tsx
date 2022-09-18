4aaf8059c7d5627fc1bec5328c946393624663d0
owen <cockyblind@gmail.com>
2022-09-18 18:58:39 +0900

    Reafactor: formref 제거

--- src/components/home/CommentForm.tsx
+++ src/components/home/CommentForm.tsx
@@ -21,7 +21,6 @@ function CommentForm({
   resetFormValue,
   formInputs,
 }: ICommentForm) {
-  const formRef = useRef<HTMLFormElement>(null);
   const [addComment] = useAddCommentMutation();
   const [updateComment] = useUpdateCommentMutation();
 
@@ -58,7 +57,7 @@ function CommentForm({
 
   return (
     <FormStyle>
-      <form onSubmit={handleForm} ref={formRef}>
+      <form onSubmit={handleForm}>
         <input
           type="hidden"
           name="id"
